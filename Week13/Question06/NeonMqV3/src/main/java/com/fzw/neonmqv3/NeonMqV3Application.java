979363c91b080e6bd0ed15f62022389a88a4e809
CarlaF <idea@idea.com>
2021-08-19 16:54:53 +0800

    week13补交-2

---
+++ Week13/Question06/NeonMqV3/src/main/java/com/fzw/neonmqv3/NeonMqV3Application.java
@@ -0,0 +1,13 @@
+package com.fzw.neonmqv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonMqV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeonMqV3Application.class, args);
+    }
+
+}
